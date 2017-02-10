a5b9a39f8186ba1be73a872fa64c1db1375b78b2
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-02-10 22:40:30 +0700

    -_-

--- blog/posts/urls.py
+++ blog/posts/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.post_home, name='index'),
-    url(r'^$', views.post_create, name='create'),
+    url(r'^create/$', views.post_create, name='create'),
 ]
