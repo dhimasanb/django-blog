d6cb0d4158582e553d314d1eac36a8c8635fc80e
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-02-10 21:50:24 +0700

    tambah list filter

--- blog/posts/admin.py
+++ blog/posts/admin.py
@@ -7,6 +7,7 @@ class PostModelAdmin(admin.ModelAdmin):
     list_display = ["title", "updated", "timestamp"]
     list_display_links = ["updated"]
     list_editable = ["title"]
+    list_filter = ["updated", "timestamp"]
     class Meta:
         model = Post
 
