4f0cca9c04702288816b75a943e9527aa954867d
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-02-10 22:43:17 +0700

    tambah route delete

--- blog/posts/urls.py
+++ blog/posts/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     url(r'^create/$', views.post_create, name='create'),
     url(r'^detail/$', views.post_create, name='detail'),
     url(r'^update/$', views.post_create, name='update'),
+    url(r'^delete/$', views.post_create, name='delete'),
 ]
