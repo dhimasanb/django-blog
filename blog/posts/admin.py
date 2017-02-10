5c52e69126f402e10882b3b9dc53d61284d0d56d
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-02-10 21:47:54 +0700

    tambah postmodel admin list display

--- blog/posts/admin.py
+++ blog/posts/admin.py
@@ -3,4 +3,9 @@ from django.contrib import admin
 # Register your models here.
 from .models import Post
 
-admin.site.register(Post)
+class PostModelAdmin(admin.ModelAdmin):
+    list_display = ["title", "updated", "timestamp"]
+    class Meta:
+        model = Post
+
+admin.site.register(Post, PostModelAdmin)
