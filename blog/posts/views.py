da5b774e854866857f41cb0b49830005b611c746
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-02-10 22:38:37 +0700

    tambah def create

--- blog/posts/views.py
+++ blog/posts/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 # Create your views here.
 def post_list(request):
     return HttpResponse("<h1>Post List</h1>")
+
+def post_create(request):
+    return HttpResponse("<h1>Post List</h1>")
