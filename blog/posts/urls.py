2833766662bf8d771f79891ffec4b2889268287f
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-02-10 22:44:32 +0700

    ngeselin -_-

--- blog/posts/urls.py
+++ blog/posts/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.post_home, name='index'),
+    url(r'^$', views.post_list, name='index'),
     url(r'^create/$', views.post_create, name='create'),
     url(r'^detail/$', views.post_detail, name='detail'),
     url(r'^update/$', views.post_update, name='update'),
