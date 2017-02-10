a62046584530cca33f7d999ad4e89ff16958ed46
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-02-11 06:22:41 +0700

    update template context system

--- blog/posts/views.py
+++ blog/posts/views.py
@@ -2,14 +2,30 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-def post_list(request):
-    return render(request, "posts/index.html", {})
+def post_list(request): #list items
+    context = {
+        "title": "List"
+    }
+
+    # if request.user.is_authenticated():
+    #     context = {
+    #         "title": "My User List"
+    #     }
+    # else:
+    #     context = {
+    #         "title": "List"
+    #     }
+
+    return render(request, "posts/index.html", context)
 
 def post_create(request):
     return HttpResponse("<h1>Create</h1>")
 
-def post_detail(request):
-    return HttpResponse("<h1>Detail</h1>")
+def post_detail(request): #retrieve
+    context = {
+        "title": "Detail"
+    }
+    return render(request, "posts/index.html", context)
 
 def post_update(request):
     return HttpResponse("<h1>Update</h1>")
