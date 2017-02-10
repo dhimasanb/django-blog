94a09d02fa241a9be602266c04f23c5cafb1c4a8
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-02-10 22:37:23 +0700

    post home ke post list

--- blog/posts/views.py
+++ blog/posts/views.py
@@ -2,5 +2,5 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-def post_home(request):
-    return HttpResponse("<h1>Hello</h1>")
+def post_list(request):
+    return HttpResponse("<h1>Post List</h1>")
