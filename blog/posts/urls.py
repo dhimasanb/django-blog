5fa64c823a59a11653d8e2b3233cb82cde350153
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-02-10 22:41:04 +0700

    tambah route post detail

--- blog/posts/urls.py
+++ blog/posts/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.post_home, name='index'),
     url(r'^create/$', views.post_create, name='create'),
+    url(r'^detail/$', views.post_create, name='detail'),
 ]
