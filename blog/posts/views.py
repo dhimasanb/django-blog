fd50b26f820d775b8609ed2ec7db860588382818
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-02-14 13:08:33 +0700

    pagination by queryset

--- blog/posts/views.py
+++ blog/posts/views.py
@@ -1,13 +1,26 @@
 from django.contrib import messages
 from django.http import HttpResponse,HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404, redirect
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 from .models import Post
 from .forms import PostForm
 
 def post_list(request): #list items
-    queryset = Post.objects.all()
+    queryset_list = Post.objects.all() # .order_by("-timestamp")
+    paginator = Paginator(queryset_list, 5) # Show 5 post per page
+
+    page = request.GET.get('page')
+    try:
+        queryset = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        queryset = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        queryset = paginator.page(paginator.num_pages)
+
     context = {
         "object_list" : queryset,
         "title": "List"
